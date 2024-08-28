f4abb0f36e9605dfdd11ad1b0cd27296ef0e7863
techrider27 <g.sangewar27@gmail.com>
2024-08-28 21:57:03 +0530

    refactor:code minimize

--- app.js
+++ app.js
@@ -3,26 +3,29 @@ import { React, useState } from "react";
 
 const Counter = () => {
   const [counter, setCounter] = useState(0);
-
-  const increase = () => {
-    setCounter(counter + 1);
-  };
-
-  const decrease = () => {
-    if (counter === 0) {
-      setCounter(0);
-      alert("counter already reached 0");
-    } else {
-      setCounter(counter - 1);
-    }
-  };
   return (
     <div className="container">
       <div className="child">
         <h1>{counter}</h1>
         <h3>React Counter</h3>
-        <button onClick={increase}>Increase</button>
-        <button onClick={decrease}>Decrease</button>
+        <div className="flex">
+          <button
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          >
+            Increase
+          </button>
+          <button
+            onClick={() => {
+              counter === 0
+                ? alert("counter reached zero")
+                : setCounter(counter - 1);
+            }}
+          >
+            Decrease
+          </button>
+        </div>
       </div>
     </div>
   );
